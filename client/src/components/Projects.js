d7fea782eaa14ae4fd21c855fcc566edcb198a28
adrian <mojofw@gmail.com>
2019-11-12 23:18:12 +0100

    Update data

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -4,66 +4,117 @@ import hitWords from "../images/hit-words.png";
 import oldTown from "../images/oldTown.png";
 import cloudApp from "../images/cloud-app.png";
 
-const Projects = () => (
-   <section id="projects">
+const Projects = () => {
+  const [hoverHeight, setHeight] = React.useState(0);
+  React.useEffect(() => {
+    function setHover() {
+      let boxHeight = document.querySelector(".img-height");
+      boxHeight = boxHeight.clientHeight + 10;
+      setHeight(boxHeight);
+    }
+    ["load", "resize"].forEach(item => window.addEventListener(item, setHover));
+  });
+
+  return (
+    <section id="projects">
       <h2>Projekty</h2>
       <div className="projectsExamples">
-         <div className="example">
-            <a href="//oldtown.netlify.com/" target="_blank" rel="noopener noreferrer" className="thumbail">
-               <img src={oldTown} alt="oldTown" />
-               <div className="hover-img">
-                  <i className="fas fa-eye">
-                     <p>Zobacz</p>
-                  </i>
-               </div>
-            </a>
-            <div className="text">
-               <h3>Old Town</h3>
-               <p>Projekt aplikacji księgarni, pozwalający na przeglądanie, dodawanie i usuwanie książek z koszyka.</p>
-               <a href="//github.com/AdrianTech/bookStore" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-github" />
-                  Zobacz kod na Github
-               </a>
+        <div className="example">
+          <a
+            href="https://bookstore01.herokuapp.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="thumbail"
+          >
+            <img className="img-height" src={oldTown} alt="oldTown" />
+            <div className="hover-img" style={{ height: hoverHeight }}>
+              <i className="fas fa-eye">
+                <p>Zobacz</p>
+              </i>
             </div>
-         </div>
-         <div className="example">
-            <a href="//hit-words.netlify.com/" target="_blank" rel="noopener noreferrer" className="thumbail">
-               <img src={hitWords} alt="hitWords" />
-               <div className="hover-img">
-                  <i className="fas fa-eye">
-                     <p>Zobacz</p>
-                  </i>
-               </div>
+          </a>
+          <div className="text">
+            <h3>Old Town</h3>
+            <p style={{ fontSize: "16px" }}>
+              Projekt fullstackowej aplikacji księgarni{" "}
+              <span style={{ fontSize: "14px" }}>
+                (CRUD - Create, Read, Update, Delete)
+              </span>
+              , pozwalający na zarządzanie treścią zarówno po stronie
+              użytkownika, jak i po stronie administratora z pomocą CMS-a
+              (Content Management System). Aplikacja załaduje się w ciągu ok. 20
+              sekund.
+            </p>
+            <a
+              href="//github.com/AdrianTech/bookStore"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github" />
+              Zobacz kod na Github
             </a>
-            <div className="text">
-               <h3>Hit Words</h3>
-               <p>Gra polegająca na wpisywaniu na czas wyświetlanego słowa w języku angielskim.</p>
-               <a href="//github.com/AdrianTech/hitWords" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-github" />
-                  Zobacz kod na Github
-               </a>
+          </div>
+        </div>
+        <div className="example">
+          <a
+            href="//hit-words.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="thumbail"
+          >
+            <img src={hitWords} alt="hitWords" />
+            <div className="hover-img" style={{ height: hoverHeight }}>
+              <i className="fas fa-eye">
+                <p>Zobacz</p>
+              </i>
             </div>
-         </div>
-         <div className="example">
-            <a href="//cloud-app.netlify.com/" target="_blank" rel="noopener noreferrer" className="thumbail">
-               <img src={cloudApp} alt="cloudApp" />
-               <div className="hover-img">
-                  <i className="fas fa-eye">
-                     <p>Zobacz</p>
-                  </i>
-               </div>
+          </a>
+          <div className="text">
+            <h3>Hit Words</h3>
+            <p>
+              Gra polegająca na wpisywaniu na czas wyświetlanego słowa w języku
+              angielskim.
+            </p>
+            <a
+              href="//github.com/AdrianTech/hitWords"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github" />
+              Zobacz kod na Github
             </a>
-            <div className="text">
-               <h3>Cloud App</h3>
-               <p>Prosta aplikacja wyświetalająca pogodę dla danego miasta.</p>
-               <a href="//github.com/AdrianTech/cloud-app" target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-github" />
-                  Zobacz kod na Github
-               </a>
+          </div>
+        </div>
+        <div className="example">
+          <a
+            href="//cloud-app.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="thumbail"
+          >
+            <img src={cloudApp} alt="cloudApp" />
+            <div className="hover-img" style={{ height: "98%" }}>
+              <i className="fas fa-eye">
+                <p>Zobacz</p>
+              </i>
             </div>
-         </div>
+          </a>
+          <div className="text">
+            <h3>Cloud App</h3>
+            <p>Prosta aplikacja wyświetalająca pogodę dla danego miasta.</p>
+            <a
+              href="//github.com/AdrianTech/cloud-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github" />
+              Zobacz kod na Github
+            </a>
+          </div>
+        </div>
       </div>
-   </section>
-);
+    </section>
+  );
+};
 
 export default Projects;
--- client/src/context.js
+++ client/src/context.js
@@ -3,133 +3,153 @@ import React, { Component, createContext } from "react";
 export const Context = createContext();
 
 export class ContextProvider extends Component {
-   state = {
-      isClicked: false,
-      moveNavbar: false,
-      message: "",
-      name: "",
-      getContact: "",
-      info: {},
-      counter: 2
-   };
-   handleBtnStart = e => {
-      const navTarget = e.target.className;
-      if (navTarget === "start") window.location.hash = "about";
-   };
-   handleScroll = () => {
-      const scrollY = window.scrollY;
-      if (scrollY > 60) {
-         this.setState({
-            moveNavbar: true
-         });
-      } else {
-         this.setState({
-            moveNavbar: false
-         });
-         window.location.hash = "main";
-      }
-   };
-   componentDidMount() {
-      window.addEventListener("scroll", this.handleScroll);
-   }
-
-   changeNav = () => {
-      this.setState(prevState => ({
-         isClicked: !prevState.isClicked
-      }));
-   };
-   handleForm = e => {
-      const name = e.target.name;
-      const value = e.target.value;
+  state = {
+    isClicked: false,
+    moveNavbar: false,
+    message: "",
+    name: "",
+    getContact: "",
+    info: {},
+    counter: 2
+  };
+  handleBtnStart = e => {
+    const navTarget = e.target.className;
+    if (navTarget === "start") window.location.hash = "about";
+  };
+  handleScroll = () => {
+    const scrollY = window.scrollY;
+    if (scrollY > 60) {
       this.setState({
-         [name]: value
+        moveNavbar: true
       });
-   };
+    } else {
+      this.setState({
+        moveNavbar: false
+      });
+      window.location.hash = "main";
+    }
+  };
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
 
-   handleSubmit = e => {
-      e.preventDefault();
-      const { name, getContact, message, counter } = this.state;
-      if (!this.validateForm()) return;
-      const data = {
-         name,
-         getContact,
-         message
-      };
-      fetch("/sendMail", {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/json"
-         },
-         body: JSON.stringify(data)
+  changeNav = () => {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+  };
+  handleForm = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, getContact, message, counter } = this.state;
+    if (!this.validateForm()) return;
+    const data = {
+      name,
+      getContact,
+      message
+    };
+    fetch("/sendMail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => {
+        if (res.ok) {
+          this.setState({
+            info: { txt: "Twoja wiadomość została wysłana", response: true },
+            name: "",
+            message: "",
+            getContact: ""
+          });
+          this.handleTimeout();
+          return res;
+        } else {
+          this.setState({
+            info: {
+              txt: "Coś poszło nie tak, spróbuj ponownie",
+              response: false
+            },
+            counter: counter + 1
+          });
+          if (counter > 3) {
+            this.setState({
+              info: {
+                txt:
+                  "Występuje jakiś błąd. Proszę, skontaktuj się ze mną telefonicznie",
+                response: false
+              }
+            });
+          }
+          this.handleTimeout();
+        }
       })
-         .then(res => {
-            if (res.ok) {
-               this.setState({
-                  info: { txt: "Twoja wiadomość została wysłana", response: true },
-                  name: "",
-                  message: "",
-                  getContact: ""
-               });
-               this.handleTimeout();
-               return res;
-            } else {
-               this.setState({
-                  info: { txt: "Coś poszło nie tak, spróbuj ponownie", response: false },
-                  counter: counter + 1
-               });
-               if (counter > 3) {
-                  this.setState({
-                     info: { txt: "Występuje jakiś błąd. Proszę, skontaktuj się ze mną telefonicznie", response: false }
-                  });
-               }
-               this.handleTimeout();
-            }
-         })
-         .catch(err => err);
-   };
-   handleTimeout = () => {
-      setTimeout(() => {
-         this.setState({
-            info: {}
-         });
-      }, 3500);
-   };
-   validateForm = () => {
-      const { message, name } = this.state;
-      if (name.trim().length < 2 || message.trim().length < 10) {
-         this.setState({
-            info: { txt: "Za krótka wiadomość lub imię", response: false }
-         });
-         this.handleTimeout();
-         return false;
-      } else {
-         return true;
-      }
-   };
+      .catch(err => err);
+  };
+  handleTimeout = () => {
+    setTimeout(() => {
+      this.setState({
+        info: {}
+      });
+    }, 3500);
+  };
+  validateForm = () => {
+    const { message, name } = this.state;
+    if (name.trim().length < 2 || message.trim().length < 10) {
+      this.setState({
+        info: { txt: "Zbyt krótka wiadomość lub imię", response: false }
+      });
+      this.handleTimeout();
+      return false;
+    } else {
+      return true;
+    }
+  };
 
-   render() {
-      const { isClicked, moveNavbar, message, getContact, name, info } = this.state;
-      const { handleBtnStart, changeNav, handleScroll, handleForm, handleSubmit } = this;
+  render() {
+    const {
+      isClicked,
+      moveNavbar,
+      message,
+      getContact,
+      name,
+      info
+    } = this.state;
+    const {
+      handleBtnStart,
+      changeNav,
+      handleScroll,
+      handleForm,
+      handleSubmit
+    } = this;
 
-      return (
-         <Context.Provider
-            value={{
-               isClicked,
-               moveNavbar,
-               name,
-               getContact,
-               message,
-               info,
-               handleForm,
-               handleSubmit,
-               handleBtnStart,
-               changeNav,
-               handleScroll
-            }}
-         >
-            {this.props.children}
-         </Context.Provider>
-      );
-   }
+    return (
+      <Context.Provider
+        value={{
+          isClicked,
+          moveNavbar,
+          name,
+          getContact,
+          message,
+          info,
+          handleForm,
+          handleSubmit,
+          handleBtnStart,
+          changeNav,
+          handleScroll
+        }}
+      >
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
 }
 export default ContextProvider;
